0ba684c6909e68bc53ede3902c55d1cb869ff317
JGeanca <geancarlo.riverahernandez@ucr.ac.cr>
2021-08-24 12:56:28 -0600

    Se añadió la carpeta de tareas

---
+++ Tareas/Tarea 1/Estudiante.java
@@ -0,0 +1,15 @@
+public class Estudiante {
+   public static void main(String argv[]){
+     String nombre = "Geancarlo Rivera";
+     System.out.println("Nombre: "+ nombre);
+	 String carnet = "C06516";
+	 System.out.println("Carnet: "+ carnet);
+     int edad = 20;
+     System.out.println("Edad: "+ edad);
+	 boolean rock = true;
+     System.out.println("Le gusta el Rock: "+ rock);
+	 double peso = 59.8;
+     System.out.println("Peso: "+ peso+ "kg");
+     System.exit(0);    
+   }
+}
