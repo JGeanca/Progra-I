46eae2b4edf2d9790be4434c0e0a44422f0782d0
GEANCARLO RIVERA HERNANDEZ <geancarlo.riverahernandez@ucr.ac.cr>
2021-08-24 15:40:15 -0600

    Se agregó la práctica de caracteres

---
+++ Caracteres.java
@@ -0,0 +1,12 @@
+public class Caracteres {
+   public static void main(String arg[]){
+   System.out.println("Nombre\tApellido\tMonto\nEmilia\tRivera\t\t\u00A245000");
+   System.out.println("Pedro\tRojas\t\t\u00A2453800");
+   System.out.println("Natalia\tSalazar\t\t\u00A25200");
+
+   
+   
+   
+   System.exit(0);
+   }
+}
